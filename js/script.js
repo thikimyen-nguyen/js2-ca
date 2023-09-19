554477685ca8941440486a1c52faab03b5c6c658
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-09-19 22:44:08 +0200

    add create account page and script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// landing page
 
+const loginForm = document.querySelector(".login");
+const createAccountForm = document.querySelector(".create-acc");
+const switchToCreateAccBtn = document.querySelector(".switch-create-acc-btn");
+const swithToLoginBtn = document.querySelector(".switch-login-btn");
+createAccountForm.classList.add("d-none");
+switchToCreateAccBtn.onclick = function switchToCreateAcc() {
+    loginForm.classList.add("d-none");
+    createAccountForm.classList.remove("d-none");
+};
+swithToLoginBtn.onclick = function () {
+    loginForm.classList.remove("d-none");
+    createAccountForm.classList.add("d-none");
+}
