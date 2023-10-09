211129751b444b2c303be22ea334d17d05c978a6
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-10-09 14:37:23 +0200

    add create post to profile page

--- js/profile.mjs
+++ js/profile.mjs
@@ -2,14 +2,25 @@ import { showUserName, showTitle, showAvatar, currentUserName } from "./profile/
 import { createPostForm, postForm} from "./posts/create-new-post.mjs";
 import { createPost } from "./components/fetch-token.mjs";
 import { showPosts, postsContentContainer } from "./posts/view-feed-posts.mjs";
-import { profileUrl, authorParam} from "./components/api-url.mjs";
+import { profileUrl, authorParam, createPostUrl} from "./components/api-url.mjs";
 // show current user
 showUserName();
 showTitle();
 showAvatar();
 
-// create new post not yet
+// show form to create post
 createPostForm();
+
+// get form input and post data to API
+postForm.addEventListener("submit", function getFormValue(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const post = Object.fromEntries(formData.entries())
+    createPost(createPostUrl, post);
+    alert("Your post was created!");
+    window.location.reload();
+  })
 
 // show user posts only
 const currentUserUrl = `${profileUrl}/${currentUserName}/posts${authorParam}`;
