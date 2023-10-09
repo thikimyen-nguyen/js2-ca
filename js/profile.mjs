5e9a9b7a6fd0007309dfe63f9edc34b7a8058a19
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-10-09 21:56:17 +0200

    fix tags input value as array

--- js/profile.mjs
+++ js/profile.mjs
@@ -16,7 +16,11 @@ postForm.addEventListener("submit", function getFormValue(event) {
     event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
-    const post = Object.fromEntries(formData.entries())
+    // Convert the "tags" value to an array
+    const tags = formData.get("tags");
+    const tagsArray = tags.split(',').map(tag => tag.trim());
+    const post = Object.fromEntries(formData.entries());
+    post.tags = tagsArray;
     createPost(createPostUrl, post);
     alert("Your post was created!");
     window.location.reload();
