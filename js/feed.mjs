cf46dede8cdac1f803cd683c8decb2be00962c8d
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-10-09 21:25:32 +0200

    fix tags value as an array

--- js/feed.mjs
+++ js/feed.mjs
@@ -35,7 +35,14 @@ postForm.addEventListener("submit", function getFormValue(event) {
     event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
-    const post = Object.fromEntries(formData.entries())
+
+    // Convert the "tags" value to an array
+    const tags = formData.get("tags");
+    const tagsArray = tags.split(',').map(tag => tag.trim());
+
+    const post = Object.fromEntries(formData.entries());
+    post.tags = tagsArray;
+    console.log(post);
     createPost(createPostUrl, post);
     alert("Your post was created!");
     window.location.reload();
