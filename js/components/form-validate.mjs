b8ddb5f19e6e25d8438fa120e0b3f66a778f8599
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-10-12 23:14:26 +0200

    add jsdoc and fix username pattern

--- js/components/form-validate.mjs
+++ js/components/form-validate.mjs
@@ -1,4 +1,4 @@
-// Create account form
+
 const userName = document.querySelector("#username");
 const nameError = document.querySelector("#name_error");
 const email = document.querySelector("#email-register");
@@ -8,8 +8,16 @@ const passwordError = document.querySelector("#password_error");
 const emailLogin = document.querySelector("#email-login");
 const emailLoginError = document.querySelector("#email-login-error");
 const loginPassword = document.querySelector("#login-password");
-// Create accoutn form
-// Validate username
+
+// Validate register form
+
+/**
+ * To check length of username and password
+ * @date 2023-10-12
+ * @param {string} value This is a value from the form input
+ * @param {number} len This is a number to check the length of the value
+ * @returns {boolean} Return a true/false of comparison
+ */
 function checkLength(value, len) {
     if (value.trim().length > len) {
         return true;
@@ -17,10 +25,16 @@ function checkLength(value, len) {
         return false;
     }
 }
+
+/**
+ * Validate username input whether is has max 20 characters and contain punctuation symbols apart from underscore (_) 
+ * @param {string} userName This is input from username
+ * @returns {boolean} Return true and set error shown as none, return false and set error shown as block
+ */
 function validateUserName(userName) {
     const regEx = /^[\w]+$/;
-    const patternMatches = regEx.test(email.value);
-    if (checkLength(userName.value, 20) !== true && patternMatches !== true) {
+    const patternMatches = regEx.test(userName.value);
+    if (checkLength(userName.value, 20) !== true && patternMatches === true) {
         nameError.style.display = "none";
         return true;
        
@@ -30,7 +44,14 @@ function validateUserName(userName) {
        
     }
 }
-// Validate register email
+
+
+/**
+ * Validate email with @stud.noroff.no or @noroff.no can register
+ * @date 2023-10-12
+ * @param {string} email This is input from email form
+ * @returns {boolean} Return true and set error shown as none, return false and set error shown as block
+ */
 function validateEmail(email) {
     const regEx = /^[\w\-.]+@(stud\.)?noroff\.no$/;
     const patternMatches = regEx.test(email.value);
@@ -45,8 +66,14 @@ function validateEmail(email) {
     }
 }
 
-// Validate password
 
+
+/**
+ * Validate password to has minimum 08 characters
+ * @date 2023-10-12
+ * @param {string} password This is input from password form
+ * @returns {boolean} Return true and set error shown as none, return false and set error shown as block
+ */
 function validatePassword(password) {
   
     if (checkLength(password.value, 7) === true) {
@@ -58,21 +85,6 @@ function validatePassword(password) {
     }
 }
 
-// log in form
 
-// validate email login
-// function validateLoginEmail(emailLogin) {
-//     const regEx = /\S+@stud.noroff\.no$/;
-//     const patternMatches = regEx.test(emailLogin.value);
- 
-//     if (patternMatches === true) {
-       
-//         emailLoginError.style.display = "none";
-//         return true;
-//     } else {
-//         emailLoginError.style.display = "block";
-//         return false;
-//     }
-// }
 
 export {validateUserName, validateEmail, validatePassword, userName, email, password, emailLogin, loginPassword }
